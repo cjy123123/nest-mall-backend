bb22c9d064e7fb34f8b839051d6f2573d49357ed
cjy <3152108075@qq.com>
2025-07-22 17:38:37 +0800

    🐞 fix(upload): 修复头像路径问题

--- src/modules/upload/upload.service.ts
+++ src/modules/upload/upload.service.ts
@@ -8,6 +8,17 @@ export class UploadService {
     const suffix = file.originalname.split('.').pop()
 
     return {
+      url: file.path.replace('uploads', ''),
+      fileSize: file.size,
+      fileType: suffix,
+    }
+  }
+
+  uploadAvatar({ file }: FileUploadDto) {
+    // 截取扩展名
+    const suffix = file.originalname.split('.').pop()
+
+    return {
       url: file.path.replace('uploads/avatar', ''),
       fileSize: file.size,
       fileType: suffix,
